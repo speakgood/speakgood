14f926d545703ed9b09604731e4ef30d70781bf6
speakgood <speakgoodGV@gmail.com>
2017-01-12 08:25:54 -0200

    Add files via upload

---
+++ speakgood/script/cadastro.php
@@ -0,0 +1,10 @@
+<?php
+
+$nome = $_POST ['nome'];
+$sobrenome = $_POST ['sobrenome'];
+$email = $_POST ['email'];
+$password = $_POST ['password'];
+$sexo = $_POST["sexo"];</code-->
+$dia,$mes,$ano =  $_POST ['dia','mes','ano']
+$connect = mysqli_connect('localhost','root', '');
+$db =
